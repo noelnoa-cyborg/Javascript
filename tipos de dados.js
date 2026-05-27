ba83a164fe18092de7d0ca0c29f1b19148ba0db0
noelnoa-cyborg <felipereginattocostasenac@gmail.com>
2026-05-27 17:22:46 -0300

    tipos de dados não primitivos

---
+++ tipos de dados.js
@@ -0,0 +1,9 @@
+let frutas = ["maça", "banana","laranja"];
+let numeros= [1,2,3,4,5];
+let misto = [true, "texto", 42];
+
+let pessoa = {
+    nome: "carlos",
+    idade: "28",
+    casado: "true"
+}
